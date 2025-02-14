d0d83c0cd550a247e72fd22ede6ef370b1477ed0
svit7 <svit7kucherenko@gmail.com>
2025-02-14 20:12:49 +0200

    My hometask "GlobalExcreptionHandler and Data-Validation"

---
+++ src/main/java/mate/academy/spring/online/bookstore/validation/IsbnValidator.java
@@ -0,0 +1,19 @@
+package mate.academy.spring.online.bookstore.validation;
+
+import jakarta.validation.ConstraintValidator;
+import jakarta.validation.ConstraintValidatorContext;
+
+public class IsbnValidator implements ConstraintValidator<Isbn, String> {
+    private static final String ISBN_PATTERN =
+            "^(?=(?:\\D*\\d){10}(?:(?:\\D*\\d){3})?$)[\\d-]+$";
+
+    @Override
+    public boolean isValid(String s, ConstraintValidatorContext context) {
+        if (s == null) {
+            return false;
+        }
+        boolean isValid = s.matches(ISBN_PATTERN);
+        System.out.println("ISBN validation result: " + isValid);
+        return isValid;
+    }
+}
