ad39855f6f42ce5b97a882e7971088cec745e4ff
svit7 <svit7kucherenko@gmail.com>
2025-03-06 09:10:45 +0200

    Logging for "Spring-Boot-Sequrity"

--- src/main/java/mate/academy/spring/online/bookstore/service/UserServiceImpl.java
+++ src/main/java/mate/academy/spring/online/bookstore/service/UserServiceImpl.java
@@ -8,6 +8,7 @@ import mate.academy.spring.online.bookstore.mapper.UserMapper;
 import mate.academy.spring.online.bookstore.model.User;
 import mate.academy.spring.online.bookstore.repository.UserRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @RequiredArgsConstructor
 @Service
@@ -16,13 +17,25 @@ public class UserServiceImpl implements UserService {
     private final UserMapper userMapper;
 
     @Override
+    @Transactional
     public UserResponseDto register(UserRegistrationRequestDto requestDto)
             throws RegistrationException {
+        System.out.println("Checking if email exists: " + requestDto.getEmail());
+
         if (userRepository.existsByEmail(requestDto.getEmail())) {
-            throw new RegistrationException("Such email already exists");
+            System.out.println("Email already exists: " + requestDto.getEmail());
+            throw new RegistrationException("Email already exists");
         }
+        System.out.println("Email is unique. Proceeding "
+                + "with saving the user: " + requestDto.getEmail());
+
         User user = userMapper.requestDtoToUser(requestDto);
-        userRepository.save(user);
+        System.out.println("User object before save: " + user);
+
+        user = userRepository.save(user);
+        System.out.println("User saved with ID: " + user.getId());
+
         return userMapper.userToUserDto(user);
     }
 }
+
