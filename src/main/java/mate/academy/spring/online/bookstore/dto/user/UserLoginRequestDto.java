2d83bfe4a7785686bd4a38d427e63329215c4999
svit7 <svit7kucherenko@gmail.com>
2025-03-21 20:54:27 +0200

    My changes for hometask "JWT"

--- src/main/java/mate/academy/spring/online/bookstore/dto/user/UserLoginRequestDto.java
+++ src/main/java/mate/academy/spring/online/bookstore/dto/user/UserLoginRequestDto.java
@@ -1,16 +1,16 @@
 package mate.academy.spring.online.bookstore.dto.user;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 public record UserLoginRequestDto(
-        @NotEmpty
+        @NotBlank
         @Size(max = 255)
         @Email
         String email,
 
-        @NotEmpty
+        @NotBlank
         @Size(max = 255)
         String password
 ) {
